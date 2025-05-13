6a6771a25a1da95c3e1b94923bc19682dfb13f99
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-13 17:53:52 +0000

    coversions study

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+//let score ="33abc"
+//let score =null
+//let score =NaN
+let score =true
+console.log(typeof score)
+console.log(typeof (score))
+
+let value=Number(score)
+console.log(typeof value);
+console.log(value)
+
+//"33"=>33
+//"33ab"=NaN
+//true=>1
+let logIn=1
+let booleanLogIn=Boolean(logIn)
+console.log(booleanLogIn)
+//1=>true;0=>false
+//""=>false
+//"nayan"=>true
+
+let num=22
+let stringnum= String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
