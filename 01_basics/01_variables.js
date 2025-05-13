7d9cedd4f86a3a209cdae2f2818c16638e4d1ba7
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-13 16:47:25 +0000

    datatype and variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1444553
+let accountEmail="juak@gmail.com"
+var accountPassword="12345"
+acccoutCity="Jaipur"
+let accountState
+
+//accountId=2 not allowed
+accountEmail="hgd@gmail.com"
+accountPassword="2121"
+acccoutCity="lol"
+console.log(accountId)
+console.table([accountId, accountEmail,accountPassword,acccoutCity,accountState])
+/*prefer not use var because of scope dikkat*/
---
+++ 01_basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"//treat all JS code as newer version
+//alert(3+3) we re using nodejs not browser
+
+console.log(3+3)
+let name="jiya"
+let age=18
+let isLoggedIn=false
+// number=> 2 to the power 53
+//bigint
+//string=""
+//boolean=> true/false
+//null =>stand alone value
+//undefined 
+//symbol=?unique
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undifiend
