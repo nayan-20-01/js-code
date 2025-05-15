90f233d58b93111e8cc08f71de14eae81abf7173
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-15 12:52:04 +0530

    operations notes added

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -23,3 +23,36 @@ let num=22
 let stringnum= String(num)
 console.log(stringnum)
 console.log(typeof stringnum)
+
+//********operations*******/
+
+
+let val=3
+let negval=-val
+console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let st1="hi"
+let st2=" pal"
+let st3=st1+st2
+console.log(st3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+"2"+2)
+console.log(1 + 2+ "2")
+
+console.log(+true)
+
+let num1,num2,num3
+
+let gamecount=100
+gamecount++
+console.log(gamecount)
