74ee714f87fb8fd4bd0f3174aeb4127ec8c6ecf2
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-19 02:02:28 +0530

    array

--- 02_basics/01array.js
+++ 02_basics/01array.js
@@ -1,1 +1,33 @@
-const myArr=[1,2,3,4, true, "nayan"]
+const myArr=[1,2,3,4, true, "nayan"] //resizable
+//copy in array are shallow copy
+console.log(myArr)
+const arr =new Array(1,2,3,4)
+
+//ARRAY METHOD
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+//myArr.unshift(0)//to add at start
+myArr.shift()
+
+
+console.log(myArr)
+console.log(myArr.includes(6))//gives answer in T or F(boolean)
+console.log(myArr.indexOf(9))
+console.log(myArr.indexOf(3))
+
+const newA=myArr.join()//adds array into stribg
+
+console.log(typeof newA)
+console.log(newA)
+
+//slice, splice
+ 
+console.log("A ", myArr)
+const myn1= myArr.slice(1,3)
+console.log(myn1)
+console.log("B ", myArr)
+
+const myn2= myArr.splice(1,3)
+console.log(myn2)
+console.log("C ", myArr)
