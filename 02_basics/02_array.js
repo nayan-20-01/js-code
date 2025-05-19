78176cfa124ac159f5e3100519d8c77f26fb8f84
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-19 12:34:44 +0530

    array part 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,17 @@
+const marvel =["thor", "iron","spider"]
+const dc=["super", "flash", "bat"]
+
+// marvel.push(dc)//array in array
+const allhero=marvel.concat(dc)//reurns a new array
+console.log(allhero)
+const all_new=[...marvel, ...dc]
+console.log(all_new)
+
+const arr1=[1,2,3,[4,5],6,7,[4,7,[2,3]]]
+const usableArr=arr1.flat(Infinity)
+console.log(arr1)
+console.log(usableArr)
+
+console.log(Array.isArray("Nayan"))
+console.log(Array.from("nayan"))
+console.log(Array.from({name: "nayan"}))//interesting- returns empty array because dony knoq what to do
