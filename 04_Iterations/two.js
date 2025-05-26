114fa3b3cec01db7c112408c1fdc93391082f7a7
nayan-20-01 <nayanjiya201@gmail.com>
2025-05-26 23:27:55 +0530

    loops

---
+++ 04_Iterations/two.js
@@ -0,0 +1,13 @@
+// for of
+// ["","",""]
+//[{},{},{}]
+
+const arr=[1,2,3,4]
+for(const num of arr){
+    console.log(num);
+}
+
+const greetings= "hello world"
+for (const grt of greetings) {
+    console.log(`eachchar is ${grt}`);
+}
